e44db11aa968c8d8502b5b292be74f809451bbbd
Dinner-coder <sambasivareddykoki@gmail.com>
2024-05-25 13:58:42 -0500

    added required code in client3,py file

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,31 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes: 
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateCorrectly(self):
+        price_a = 120.0
+        price_b = 60.0
+        self.assertEqual(getRatio(price_a, price_b), 2.0)
+        self.assertEqual(getRatio(price_b, price_a), 0.5)
+
+  def test_getRatio_divideByZero(self):
+        price_a = 120.0
+        price_b = 0
+        self.assertIsNone(getRatio(price_a, price_b))
+
+  def test_getRatio_zeroNumerator(self):
+        price_a = 0
+        price_b = 60.0
+        self.assertEqual(getRatio(price_a, price_b), 0.0)
+
+  def test_getDataPoint_invalidData(self):
+        quote = {'top_ask': {'price': None, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        with self.assertRaises(TypeError):
+            getDataPoint(quote)
 
 
 
